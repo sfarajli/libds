99ed77a0c3caa4f8841eaffa0475d42383efed9d
Suleyman Farajli <suleyman@farajli.net>
2024-12-01 13:01:47 +0400

    stack added

---
+++ src/stack.c
@@ -0,0 +1,64 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+typedef struct node node;
+
+void
+stack_free(node *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+		stack_free(head->next);
+
+	free(head);
+}
+
+node *
+stack_init(void)
+{
+	node *head = NULL;
+	return head;
+}
+
+
+int
+stack_push(node **head, int value)
+{
+	node *new = malloc(sizeof(node));
+	if (new == NULL)
+		return 1;
+	new->value = value;
+	new->next = *head;
+	*head = new;
+	return 0;
+}
+
+int
+stack_pop(node **head)
+{
+	if (*head == NULL)
+		return INT_MIN;
+
+	int result = (*head)->value;
+	node *tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return result;
+}
+void
+stack_print(struct node *head)
+{
+	if (head == NULL)
+		return;
+
+	for (node *i = head; i != NULL; i = i->next)
+		printf("%d\n", i->value);
+}
